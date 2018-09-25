4dd4f404cee5cb54db2eabd862c19fffd93a1464
bezbee <zizu087@gmail.com>
2018-09-24 19:09:46 -0700

    Перенесла все, что касается диалога в модуль dialog

--- js/dialog.js
+++ js/dialog.js
@@ -1,5 +1,46 @@
 'use strict';
 (function () {
+  var userDialog = document.querySelector('.setup');
+  var ESC_CODE = 27;
+  var ENTER_CODE = 13;
+  var icon = document.querySelector('.setup-open');
+  var dialogCloseButton = document.querySelector('.setup-close');
+
+  var closeDialog = function () {
+    userDialog.classList.add('hidden');
+  };
+
+  var openDialog = function () {
+    userDialog.classList.remove('hidden');
+    userDialog.style.cssText = 'top: 80px; left: 50%';
+  };
+  var setupUser = document.querySelector('.setup-user-name');
+  var onEscClose = function (evt) {
+    if (evt.keyCode === ESC_CODE && document.activeElement !== setupUser) {
+      closeDialog();
+    }
+  };
+
+  icon.addEventListener('click', function () {
+    openDialog();
+
+    document.addEventListener('keydown', onEscClose);
+    dialogCloseButton.addEventListener('keydown', function (evt) {
+      if (evt.keyCode === ENTER_CODE) {
+        closeDialog();
+      }
+    });
+  });
+
+
+  dialogCloseButton.addEventListener('click', closeDialog,
+      document.removeEventListener('keydown', onEscClose));
+
+  document.querySelector('.setup-open-icon').addEventListener('keydown', function (evt) {
+    if (evt.keyCode === ENTER_CODE) {
+      openDialog();
+    }
+  });
 
   var dialogHandle = document.querySelector('.setup-user');
 
@@ -24,8 +65,8 @@
         y: moveEvt.clientY
       };
 
-      window.setup.userDialog.style.top = (window.setup.userDialog.offsetTop - shift.y) + 'px';
-      window.setup.userDialog.style.left = (window.setup.userDialog.offsetLeft - shift.x) + 'px';
+      userDialog.style.top = (userDialog.offsetTop - shift.y) + 'px';
+      userDialog.style.left = (userDialog.offsetLeft - shift.x) + 'px';
     };
 
     var onMouseUp = function (upEvt) {
