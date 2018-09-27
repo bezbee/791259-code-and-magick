c1e9d242c230ee71e3bb7d0480c48aae31642ebf
bezbee <zizu087@gmail.com>
2018-09-26 23:31:27 -0700

    deleted module colorize

--- js/setup.js
+++ js/setup.js
@@ -9,6 +9,8 @@
 
   var WIZARDS_EYE_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
 
+  var WIZARD_FIREBALL_COLOR = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
+
   var similarListElement = document.querySelector('.setup-similar-list');
 
   var similarWizardTemplate = document.querySelector('#similar-wizard-template')
@@ -22,6 +24,12 @@
   var dialogCloseButton = document.querySelector('.setup-close');
   var ESC_CODE = 27;
   var ENTER_CODE = 13;
+  var wizardCoat = document.querySelector('.wizard-coat');
+  var wizardEyes = document.querySelector('.wizard-eyes');
+  var wizardFireball = document.querySelector('.setup-fireball-wrap');
+  var inputCoatColor = document.querySelector('input[name="coat-color"]');
+  var inputEyesColor = document.querySelector('input[name="eyes-color"]');
+  var inputFireballColor = document.querySelector('input[name="fireball-color"]');
 
   var getRandomItem = function (items) {
     return items[Math.floor(Math.random() * items.length)];
@@ -57,6 +65,26 @@
   }
   similarListElement.appendChild(fragment);
 
+  wizardCoat.addEventListener('click', function () {
+    var randomCoatColor = getRandomItem(WIZARDS_COAT_COLORS);
+    wizardCoat.style.fill = randomCoatColor;
+    inputCoatColor.value = randomCoatColor;
+  });
+
+  wizardEyes.addEventListener('click', function () {
+    wizardEyes.setAttribute('style', 'fill');
+    var randomEyesColor = getRandomItem(WIZARDS_EYE_COLORS);
+    wizardEyes.style.fill = randomEyesColor;
+    inputEyesColor.value = randomEyesColor;
+  });
+
+  wizardFireball.addEventListener('click', function () {
+    wizardFireball.setAttribute('style', 'background-color');
+    var randomFireballColor = getRandomItem(WIZARD_FIREBALL_COLOR);
+    wizardFireball.style.backgroundColor = randomFireballColor;
+    inputFireballColor.value = randomFireballColor;
+  });
+
   document.querySelector('.setup-similar').classList.remove('hidden');
 
   var setupUser = document.querySelector('.setup-user-name');
@@ -76,7 +104,6 @@
       }
     });
   });
-
 
   dialogCloseButton.addEventListener('click', function () {
     window.dialog.close();
